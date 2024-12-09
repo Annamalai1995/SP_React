752157c3113e71bb7fd1e6984eca0ab0f9e01a6e
Annamalai1995 <s.annamalai1995@gmail.com>
2024-12-09 12:09:27 +0530

    Tablecss

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import List, { Unorderlist } from "./Function";
 import { Parameter } from "./Parameter";
 import { Tables } from "./Table";
+import { Travelsname } from "./Tablecss";
 import { Hook } from "./UseState";
 import { Hook1 } from "./UseState1";
 
@@ -12,8 +13,8 @@ function App() {
     <Tables/>
     <Hook/> */}
     <Hook1/>
-    <Parameter where="./download.jpg"length="200px" breadth="200px"/>
-
+    {/* <Parameter where="./download.jpg"length="200px" breadth="200px"/> */}
+<Travelsname/>
     </>
         
   )
